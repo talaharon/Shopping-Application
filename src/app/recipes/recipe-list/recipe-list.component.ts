bb10a4bf3d4d4fa397893b7421360aa843f48330
Tal Aharon <taharon@taharon-macbookpro.roam.corp.google.com>
2019-11-10 11:01:23 +0200

    Adding Content to the Recipes Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test','https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwifncW-o9_lAhUNZVAKHaUTDwIQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.thrillist.com%2Fnews%2Fnation%2Fblack-friday-food-deals-2018&psig=AOvVaw0U3Xp_XRCC8ouZ8jAC--Xn&ust=1573462703207538')
+  ]
+
 
   constructor() {
    }
