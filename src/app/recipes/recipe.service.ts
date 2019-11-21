5abfaad0d841dc67a3d288c48efa674399f7752d
Tal Aharon <taharon@google.com>
2019-11-21 14:56:08 +0200

    Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,25 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('A Test Recipe', 'This is simply a test', 
+    'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+    [
+      new Ingredient("Meat", 2),
+      new Ingredient("French Fries", 1)
+    ]),
+    new Recipe('Another Test Recipe', 
+    'This is simply a test', 
+    'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+    [
+      new Ingredient("Buns", 2),
+      new Ingredient("Meat", 1)
+    ])
   ];
 
   recipeSelected = new EventEmitter<Recipe>();
